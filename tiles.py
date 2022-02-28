de3a3318163ef11d4381ac44560ce3ec39c5d73c
Levy Mota <levy.moreira@ambevtech.com.br>
2022-02-28 09:33:58 -0300

    black changes

--- tiles.py
+++ tiles.py
@@ -7,27 +7,36 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
         self.rect.x += x_shift
+
 
 class StaticTile(Tile):
     def __init__(self, size, x, y, surface):
         super().__init__((x, y), size)
         self.image = surface
 
+
 class Grass(StaticTile):
     def __init__(self, size, x, y, surface):
         super().__init__(size, x, y, surface)
         # self.rect = self.image.get_rect(bottom = size * 2)
-        
+
 
 class Crate(StaticTile):
     def __init__(self, size, x, y):
-        super().__init__(size, x, y, pygame.image.load('./graphics/terrain/crate.png').convert_alpha())
+        super().__init__(
+            size,
+            x,
+            y,
+            pygame.image.load("./graphics/terrain/crate.png").convert_alpha(),
+        )
         offset_y = y + size
-        self.rect = self.image.get_rect(bottomleft = (x, offset_y))
+        self.rect = self.image.get_rect(bottomleft=(x, offset_y))
+
+
 
 class AnimatedTile(Tile):
     def __init__(self, size, x, y, path):
@@ -46,12 +55,13 @@ class AnimatedTile(Tile):
         self.animate()
         self.rect.x += x_shift
 
+
 class Coin(AnimatedTile):
     def __init__(self, size, x, y, path):
         super().__init__(size, x, y, path)
         center_x = x + int(size / 2)
         center_y = y + int(size / 2)
-        self.rect = self.image.get_rect(center = (center_x, center_y))
+        self.rect = self.image.get_rect(center=(center_x, center_y))
 
 
 class Palm(AnimatedTile):
